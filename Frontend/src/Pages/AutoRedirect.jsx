c73bda35e095f74a94032ff7e92a06799a9eb00a
Harsh <gamerpandeyharsh@gmail.com>
2025-05-06 17:07:29 +0530

    favicon added

--- Frontend/src/Pages/AutoRedirect.jsx
+++ Frontend/src/Pages/AutoRedirect.jsx
@@ -15,7 +15,7 @@ const AutoRedirect = () => {
       } else {
         navigate("/start");
       }
-    }, 5000); // Optional delay to show the preloader
+    }, 6000); // Optional delay to show the preloader
 
     return () => clearTimeout(timer);
   }, [navigate]);
